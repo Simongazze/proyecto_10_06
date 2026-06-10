3c7c4251e032bb6f52063002feec853893c1d721
Simongazze <simongazze@hotmail.com>
2026-06-10 13:33:40 -0300

    agrego otro gráfico

--- scripts/grafico.py
+++ scripts/grafico.py
@@ -1,4 +1,17 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 datos = pd.read_csv("titanic.csv")
+
+fig, ax= plt.subplots()
+
+g = sns.countplot(x = "Sex", hue = "Survived", data = datos, ax=ax)
+
+g.figure.savefig("plot.png")
+
+fig, ax= plt.subplots()
+
+g2 = sns.countplot(x = "Pclass", hue = "Survived", data = datos, ax=ax)
+
+g2.figure.savefig("plot2.png")
