18acb1fa9e5efcba23c909634935d698641128de
Simongazze <simongazze@hotmail.com>
2026-06-10 13:51:50 -0300

    hago una modificación

--- scripts/grafico.py
+++ scripts/grafico.py
@@ -2,3 +2,11 @@ import pandas as pd
 import seaborn as sns
 
 datos = pd.read_csv("titanic.csv")
+
+g = sns.countplot(x = "Sex", hue = "Survived", data = datos, ax=ax)
+
+g.figure.savefig("plot.png")
+
+g = sns.countplot(x = "Pclass", hue = "Survived", data = datos, ax=ax)
+
+g.figure.savefig("plot2.png")
